c249efb626b0a409f7b8e69a0c8a594297ac1f14
wayls <minesuperlove@163.com>
2017-11-17 00:56:40 +0800

    SpringBoot整合Spring MVC实现HelloWorld

---
+++ src/test/java/site/wayls/SpringbootMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package site.wayls;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
